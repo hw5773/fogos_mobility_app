d139783b30eef1be7213e108cde0a16d8ba4f1fd
mckwak <mckwak@mmlab.snu.ac.kr>
2019-08-29 16:02:23 +0900

    apply the new .gitignore

--- app/src/main/java/project/versatile/FogOSMobilityClient.java
+++ app/src/main/java/project/versatile/FogOSMobilityClient.java
@@ -8,7 +8,7 @@ import FogOSMessage.RequestMessage;
 import FogOSMessage.ResponseMessage;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
